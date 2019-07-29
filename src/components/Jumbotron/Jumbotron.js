c3a2eb1b2cba71254424a839d7317416cf5739f2
chrisbrady510 <chrismbrady@gmail.com>
2019-07-28 18:56:31 -0700

    It's me Mario

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Jumbotron.css";
+
+
+const Jumbotron = () => (
+<div className="jumbotron jumbotron-fluid">
+  <div className="container text-center">
+    <h1 className="display-4">React Clicky Game!</h1>
+  </div>
+</div>
+);
+
+export default Jumbotron;
